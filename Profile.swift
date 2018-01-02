f7a893162b120264f724e1477f378cbda76e4630
Amanda Goodhines <34574951+amandagoodhines@users.noreply.github.com>
2018-01-01 22:27:02 -0500

    Update Profile.swift
    Version1:added ability for the data saved to be stored onto the device.
    changes may need to be made regarding the init? statement and which variables are optionals

--- Profile.swift
+++ Profile.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
-class Profile{
-    //MARK: Properties
+import og.log
+class Profile: NSObject, NSCoding {
     
+    //MARK: Properties
     var name: String
     var age: String
     var sport: String
@@ -18,6 +19,10 @@ class Profile{
     var weight: String
     var userID: String
     
+    //MARK: Archiving Paths
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("profiles")
+    
     //MARK: Initialization
     init?(name:String, age:String, sport:String, photo:UIImage?, height: String, weight: String, userID: String){
         
@@ -25,15 +30,10 @@ class Profile{
         guard !name.isEmpty else {
             return nil
         }
-        /*
-        //the age has to be a positive number
-        guard (age > 0)  else{
-            return nil
-        }*/
+       
     
     
     // Initialize stored properties
-
     self.name = name
     self.age = age
     self.sport = sport
@@ -41,7 +41,58 @@ class Profile{
     self.height = height
     self.weight = weight
     self.userID = userID
-
     }
     
+    //MARK: NSCoding
+    func encode(with aCoder: NSCoder) {
+        aCoder.encode(name, forKey: PropertyKey.name)
+        aCoder.encode(age, forKey: PropertyKey.age)
+        aCoder.encode(sport, forKey: PropertyKey.sport)
+        aCoder.encode(photo, forKey: PropertyKey.photo)
+        aCoder.encode(height, forKey: PropertyKey.height)
+        aCoder.encode(weight, forKey: PropertyKey.weight)
+        aCoder.encode(userID, forKey: PropertyKey.userID)
+    }
+    
+    
+    //note: look into which variables are optionals. and if they need to be unwinded (which requires the guard & else statement below)
+    //other suggested solution is to not have the guard & else statement, and have a ! after the variable in the initialization statement instead. 
+    required convenience init?(coder aDecoder: NSCoder) {
+        //the name is required, so if we cannot decode a name string, the initializer should fail
+        guard let name = aDecoder.decodeObject(forKey: PropertyKey.name) as? String
+            else{
+                os_log("Unable to decode the name for a Profile object", log: OSLog.default, type: .debug)
+                return nil
+        }
+        guard let age = aDecoder.decodeObject(forKey: PropertyKey.age) as? String
+            else{
+                os_log("Unable to decode the age for a Profile object", log: OSLog.default, type: .debug)
+                return nil
+        }
+        guard let sport = aDecoder.decodeObject(forKey: PropertyKey.sport) as? String
+            else{
+                os_log("Unable to decode the sport for a Profile object", log: OSLog.default, type: .debug)
+                return nil
+        }
+        //because photo is an optional property of Profile, jsut use conditional cast
+        let photo = aDecoder.decodeObject(forKey: PropertyKey.photo) as? UIImage
+        guard let height = aDecoder.decodeObject(forKey: PropertyKey.height) as? String
+            else{
+                os_log("Unable to decode the height for a Profile object", log: OSLog.default, type: .debug)
+                return nil
+        }
+        guard let weight = aDecoder.decodeObject(forKey: PropertyKey.weight) as? String
+            else{
+                os_log("Unable to decode the weight for a Profile object", log: OSLog.default, type: .debug)
+                return nil
+        }
+        guard let userID = aDecoder.decodeObject(forKey: PropertyKey.userID) as? String
+            else{
+                os_log("Unable to decode the userid for a Profile object", log: OSLog.default, type: .debug)
+                return nil
+        }
+        
+        //Must call a designated initializer
+        self.init(name: name, age: age, sport: sport, photo: photo, height: height, weight: weight, userID: userID)
+    }
 }
